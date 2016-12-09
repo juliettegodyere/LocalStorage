96f00b1f763a6a13fb6dfbe256aa3012d4f487d2
juliettegodyere <juliettejuli@yahoo.com>
2016-12-09 16:52:57 +0100

    first commit

---
+++ localstorage.js
@@ -0,0 +1,21 @@
+function localStorage(){
+	alert("HFFGFDDD"):
+	// var testObject=[]; 
+	// item = {};       
+
+	// $("input").each(function() { 
+	//     var title = $(#box1).val();
+	//     var message = $(#box2).val();
+	//     item ["title"] = title;
+	//     item ["message"] = message;
+	// });
+	// testObject.push(item);  
+	// // Put the object into storage
+	// localStorage.setItem('testObject', JSON.stringify(testObject));
+
+	// // Retrieve the object from storage
+	// var retrievedObject = localStorage.getItem('testObject');
+
+	// // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+	// alert('retrievedObject: ', JSON.parse(retrievedObject));
+}
